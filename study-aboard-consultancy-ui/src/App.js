83135489ac7106eca9e1a60d075b2d5851762c90
GauthamAskani <89175644+GauthamAskani@users.noreply.github.com>
2024-06-18 23:05:54 +0530

    Configured Basic Setup for UI using npx create react app

---
+++ study-aboard-consultancy-ui/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          STUDY ABORAD-COUNSULTANCY
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
