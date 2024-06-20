e312857f058ca0ca31d554065baffef0e7b74dc4
reddyvishwareddy <168004210+reddyvishwareddy@users.noreply.github.com>
2024-06-20 13:05:19 +0530

    Tested the DB config setup with node js and mysql db connection

--- study-aboard-consultancy-api/index.js
+++ study-aboard-consultancy-api/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin123',
+    database: 'consultancy'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database');
+});
 
 app.get('/', (req, res) => {
     res.send('Welcome to Study Aboard Consultancy');
@@ -9,3 +25,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
