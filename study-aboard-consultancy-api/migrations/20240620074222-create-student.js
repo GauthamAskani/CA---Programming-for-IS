c6e0e23378452987ec1e05aa6b62d7eb7a7b2180
reddyvishwareddy <168004210+reddyvishwareddy@users.noreply.github.com>
2024-06-20 14:10:50 +0530

    Created the initial migration for student table

---
+++ study-aboard-consultancy-api/migrations/20240620074222-create-student.js
@@ -0,0 +1,73 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('students', {
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      student_first_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      student_family_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      student_dob: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      student_gender: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      student_country_origin: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      student_phone_number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      student_email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      student_password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      student_status: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      student_document_status: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      student_created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      student_updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      student_deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('students');
+  }
+};
+
