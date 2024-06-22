7347a16aa7b74300883d0a6ecf9fa050056c55af
Amrutha Challa <104781372+Amruthaa-b@users.noreply.github.com>
2024-06-22 11:29:35 +0100

    created medical insurance request api

---
+++ study-aboard-consultancy-api/controllers/medicalinsurance.js
@@ -0,0 +1,31 @@
+const models = require('../models')
+const moment = require('moment');
+
+async function studentRequestMedicalInsurance(data) {
+
+    try {
+        const studentExists = await models.MedicalInsurance.findOne({
+            where: { student_id: data.student_id }
+        })
+        if (studentExists) {
+            throw new Error('Student has a open medical insurance request');
+        }
+        data.cover_start_date = moment(data.cover_start_date, 'MM-DD-YYYY').format('YYYY-MM-DD');
+        data.cover_end_date = moment(data.cover_end_date, 'MM-DD-YYYY').format('YYYY-MM-DD');
+        data.course_start_date = moment(data.course_start_date, 'MM-DD-YYYY').format('YYYY-MM-DD');
+        data.course_end_date = moment(data.course_end_date, 'MM-DD-YYYY').format('YYYY-MM-DD');
+        
+        const result = await models.MedicalInsurance.create(data)
+        console.log(result);
+        return {
+            medical_insurnace_id: result.medical_insurance_id
+        };
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+module.exports = {
+    studentRequestMedicalInsurance,
+}
