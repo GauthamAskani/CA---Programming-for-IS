82f7cd1cdbe0e3882e42cebf5969b202fe5997f3
Sravanisravs04 <sravaninaidu004@gmail.com>
2024-06-24 15:13:47 +0100

    Created university table migration and model

---
+++ study-aboard-consultancy-api/migrations/20240624140819-create-universities.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Universities', {
+      university_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      university_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      university_shortname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      university_description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      university_image_url: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      university_program_intake: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      university_program_intake_status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      university_created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      university_updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      university_deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Universities');
+  }
+};
---
+++ study-aboard-consultancy-api/models/university.js
@@ -0,0 +1,64 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class University extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here if needed
+    }
+  }
+  University.init({
+    university_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    university_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    university_shortname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    university_description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    university_image_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    university_program_intake: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    university_program_intake_status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    university_created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    university_updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    university_deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  }, {
+    sequelize,
+    modelName: 'University',
+    timestamps: false,
+  });
+  return University;
+};
