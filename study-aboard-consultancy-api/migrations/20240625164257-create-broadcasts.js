efcdf6ebd6f00833b4f59dbf1e2b082ae8aaa4be
Amrutha Challa <104781372+Amruthaa-b@users.noreply.github.com>
2024-06-25 17:55:48 +0100

    Created broadcast table migration and model

---
+++ study-aboard-consultancy-api/migrations/20240625164257-create-broadcasts.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Broadcasts', {
+      broadcast_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      broadcast_title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      broadcast_image_url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      broadcast_message: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      broadcast_send_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      broadcast_expiry_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      broadcast_created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      broadcast_updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      broadcast_deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Broadcasts');
+  }
+};
+
---
+++ study-aboard-consultancy-api/models/broadcasts.js
@@ -0,0 +1,60 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Broadcast extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here if needed
+    }
+  }
+  Broadcast.init({
+    broadcast_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    broadcast_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    broadcast_image_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    broadcast_message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    broadcast_send_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    broadcast_expiry_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    broadcast_created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    broadcast_updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    broadcast_deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  }, {
+    sequelize,
+    modelName: 'Broadcast',
+    timestamps: false,
+  });
+  return Broadcast;
+};
