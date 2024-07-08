a23045bb8e68f56aa2e5891fbd620a4d385bf8d1
Sravanisravs04 <sravaninaidu004@gmail.com>
2024-07-08 11:42:47 +0530

    Created the basic route for the application

--- study-aboard-consultancy-ui/src/App.js
+++ study-aboard-consultancy-ui/src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          STUDY ABORAD-COUNSULTANCY
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div>Navbar</div>
+      <Routes>
+        <Route exact path="/" element={<div>Landing Page</div>} />
+        <Route path="/why-us" element={<div>Why us</div>} />
+        <Route path="/feature" element={<div>Feature</div>} />
+        <Route element={<div>Not Found</div>} />
+      </Routes>
+    </Router>
   );
 }
 
