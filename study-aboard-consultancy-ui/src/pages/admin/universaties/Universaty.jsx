b23e5c8a36e5b0eb80946b8c7363a92e04537e7d
Amrutha Challa <104781372+Amruthaa-b@users.noreply.github.com>
2024-07-11 22:40:39 +0530

    Created and added the listing feature for universities to admin

--- study-aboard-consultancy-ui/src/pages/admin/universaties/Universaty.jsx
+++ study-aboard-consultancy-ui/src/pages/admin/universaties/Universaty.jsx
@@ -7,117 +7,181 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-
-const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toFixed(2),
-  },
-];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
+import moment from "moment";
+import { Card, CardContent } from "@mui/material";
+import Umodal from "../../../components/universatymodal/Umodal";
+import { deleteUniversaty, getUniversatyList } from "../../../apis/universaty";
+import { toast } from "react-toastify";
+import AlertModal from "../../../components/alertModal/AlertModal";
+import { useNavigate } from "react-router-dom";
 
 export default function Universaty() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const navigate = useNavigate();
+  const [jobsData, setJobsData] = React.useState([
+    {
+      university_name: "National College of Ireland",
+      university_shortname: "NCI",
+      university_description: "The National",
+      university_program_intake: "JAN & SEP",
+      university_program_intake_status: "CLOSED",
+      id: 1,
+    },
+  ]);
+  const [modal, setModal] = React.useState(false);
+  const [activeItem, setActiveItem] = React.useState(null);
+  const [deleteModal, setDeleteModal] = React.useState(false);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
+  const header = [
+    "Name",
+    "Description",
+    "Program Intake",
+    "Intake Status",
+    "Action",
+  ];
+
+  const handleGetUniversaties = async () => {
+    try {
+      const res = await getUniversatyList();
+      console.log(("data", res));
+      setJobsData(res);
+    } catch (e) {
+      console.log("er->", e);
+    }
   };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
+  const handleDeleteUniversaties = async () => {
+    try {
+      const res = await deleteUniversaty(activeItem?.id);
+      console.log(("data", res));
+      setActiveItem(null);
+      setDeleteModal(false);
+      toast.success("Deleted successfully");
+    } catch (e) {
+      console.log("er->", e);
+    }
   };
+
+  const onConfirm = () => {
+    handleDeleteUniversaties();
+  };
+
+  const onCancel = () => {
+    setActiveItem(null);
+    setDeleteModal(false);
+  };
+
+  React.useEffect(() => {
+    handleGetUniversaties();
+  }, []);
 
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <div className="user-wrapper p-5">
+      {" "}
+      <Card
+        style={{
+          marginTop: "2rem",
+          backgroundColor: "#ffffff",
+          minHeight: "300px",
+        }}
+      >
+        <CardContent>
+          <div className="d-flex justify-content-between mb-3">
+            <h4 style={{ fontFamily: "Poppins !important", color: "orange" }}>
+              Universaties
+            </h4>
+            <button
+              type="button"
+              className="orange-button"
+              onClick={() => {
+                setModal(true);
+              }}
+            >
+              Add Universaty
+            </button>
+          </div>
+
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {header?.map((item) => (
+                    <TableCell key={item}>{item}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {jobsData && jobsData?.length ? (
+                  jobsData?.map((app, index) => (
+                    <TableRow hover key={index}>
+                      <TableCell className="title-wrapper">
+                        {app?.university_name || "National College of Ireland"}{" "}
+                        ({app.university_shortname})
+                      </TableCell>
+
+                      <TableCell>
+                        {app?.university_description || "-"}
+                      </TableCell>
+                      <TableCell>
+                        {app?.university_program_intake || "-"}
+                      </TableCell>
+                      <TableCell>
+                        {app?.university_program_intake_status || "-"}
+                      </TableCell>
+                      <TableCell>
+                        <button
+                          onClick={() => {
+                            setModal(true);
+                            setActiveItem(app);
+                          }}
+                          className="mr-2"
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="mr-2"
+                          onClick={() => {
+                            navigate(`/admin-courses?id=${app.id}`);
+                          }}
+                        >
+                          View
+                        </button>
+                        <button
+                          onClick={() => {
+                            setActiveItem(app);
+                            setDeleteModal(true);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <p className="text-center">No Data Found</p>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </CardContent>
+      </Card>
+      {modal && (
+        <Umodal
+          isOpen={modal}
+          toggle={() => {
+            setModal(false);
+          }}
+          activeItem={activeItem}
+          setActiveItem={setActiveItem}
+        />
+      )}
+      {deleteModal && (
+        <AlertModal
+          isOpen={deleteModal}
+          onCancel={onCancel}
+          onConfirm={onConfirm}
+          toggle={onCancel}
+        />
+      )}
+    </div>
   );
 }
