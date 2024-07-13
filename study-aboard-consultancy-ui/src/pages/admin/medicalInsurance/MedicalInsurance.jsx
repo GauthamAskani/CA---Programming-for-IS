b5055dff5d80b1a1dccfc1e9aab994a4384336d3
Amrutha Challa <104781372+Amruthaa-b@users.noreply.github.com>
2024-07-13 13:05:32 +0530

    Added sort feature to sort requests for admin

--- study-aboard-consultancy-ui/src/pages/admin/medicalInsurance/MedicalInsurance.jsx
+++ study-aboard-consultancy-ui/src/pages/admin/medicalInsurance/MedicalInsurance.jsx
@@ -1,45 +1,80 @@
 import * as React from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import { DataGrid } from "@mui/x-data-grid";
 import { Card, CardContent } from "@mui/material";
+import moment from "moment";
 import { getMedicalList } from "../../../apis/universaty";
 import Imodal from "../../../components/insurance/InsuranceModal";
-import moment from "moment";
 
 export default function MedicalInsurance() {
-  const [jobsData, setJobsData] = React.useState([]);
+  const [rows, setRows] = React.useState([]);
   const [modal, setModal] = React.useState(false);
   const [activeItem, setActiveItem] = React.useState(null);
-  const [deleteModal, setDeleteModal] = React.useState(false);
 
-  const header = [
-    "Id",
-    "Cover Start Date",
-    "Cover End Date",
-    "Destination Country",
-    "University Name",
-    "Course Title",
-    "Course Cost",
-    "Course Start Date",
-    "Course End Date",
-    "Student Notes",
-    "Admin Remarks",
-    "Status",
-    "Actions",
+  const columns = [
+    { field: "medical_insurance_id", headerName: "Id", width: 50 },
+    {
+      field: "cover_start_date",
+      headerName: "Cover Start Date",
+      width: 150,
+      valueFormatter: (params) =>
+        params.value ? moment(params.value).format("MM/DD/YYYY") : "-",
+    },
+    {
+      field: "cover_end_date",
+      headerName: "Cover End Date",
+      width: 150,
+      valueFormatter: (params) =>
+        params.value ? moment(params.value).format("MM/DD/YYYY") : "-",
+    },
+    {
+      field: "destination_country",
+      headerName: "Destination Country",
+      width: 200,
+    },
+    { field: "university_name", headerName: "University Name", width: 200 },
+    { field: "course_title", headerName: "Course Title", width: 200 },
+    { field: "course_cost", headerName: "Course Cost", width: 150 },
+    {
+      field: "course_start_date",
+      headerName: "Course Start Date",
+      width: 150,
+      valueFormatter: (params) =>
+        params.value ? moment(params.value).format("MM/DD/YYYY") : "-",
+    },
+    {
+      field: "course_end_date",
+      headerName: "Course End Date",
+      width: 150,
+      valueFormatter: (params) =>
+        params.value ? moment(params.value).format("MM/DD/YYYY") : "-",
+    },
+    { field: "student_notes", headerName: "Student Notes", width: 200 },
+    { field: "admin_remarks", headerName: "Admin Remarks", width: 200 },
+    { field: "status", headerName: "Status" },
+    {
+      field: "actions",
+      headerName: "Actions",
+
+      renderCell: (params) => (
+        <span
+          onClick={() => {
+            setModal(true);
+            setActiveItem(params.row);
+          }}
+          className="mr-2 button-wrapper-edit"
+        >
+          Edit
+        </span>
+      ),
+    },
   ];
 
   const handleGetMedicalInsurance = async () => {
     try {
       const res = await getMedicalList();
-      console.log(("data", res));
-      setJobsData(res);
+      setRows(res);
     } catch (e) {
-      console.log("er->", e);
+      console.log("Error:", e);
     }
   };
 
@@ -49,7 +84,6 @@ export default function MedicalInsurance() {
 
   return (
     <div className="user-wrapper p-5">
-      {" "}
       <Card
         style={{
           marginTop: "2rem",
@@ -63,68 +97,23 @@ export default function MedicalInsurance() {
               Medical Insurance
             </h4>
           </div>
-
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {header?.map((item) => (
-                    <TableCell key={item}>{item}</TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {jobsData && jobsData?.length ? (
-                  jobsData?.map((app, index) => (
-                    <TableRow hover key={index}>
-                      <TableCell>{app?.medical_insurance_id || "-"}</TableCell>
-                      <TableCell>
-                        {app?.cover_start_date
-                          ? moment(app?.cover_start_date).format("MM/DD/YYYY")
-                          : "-"}
-                      </TableCell>
-                      <TableCell>
-                        {app?.cover_end_date
-                          ? moment(app?.cover_end_date).format("MM/DD/YYYY")
-                          : "-"}
-                      </TableCell>
-                      <TableCell>{app?.destination_country || "-"}</TableCell>
-                      <TableCell>{app?.university_name || "-"}</TableCell>
-                      <TableCell>{app?.course_title || "-"}</TableCell>
-                      <TableCell>{app?.course_cost || "-"}</TableCell>
-                      <TableCell>
-                        {app?.course_start_date
-                          ? moment(app?.course_start_date).format("MM/DD/YYYY")
-                          : "-"}
-                      </TableCell>
-                      <TableCell>
-                        {app?.course_end_date
-                          ? moment(app?.course_end_date).format("MM/DD/YYYY")
-                          : "-"}
-                      </TableCell>
-                      <TableCell>{app?.student_notes || "-"}</TableCell>
-                      <TableCell>{app?.admin_remarks || "-"}</TableCell>
-                      <TableCell>{app?.status || "-"}</TableCell>
-
-                      <TableCell sx={{ minWidth: "200px" }}>
-                        <span
-                          onClick={() => {
-                            setModal(true);
-                            setActiveItem(app);
-                          }}
-                          className="mr-2 button-wrapper-edit"
-                        >
-                          Edit
-                        </span>
-                      </TableCell>
-                    </TableRow>
-                  ))
-                ) : (
-                  <p className="text-center">No Data Found</p>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <div style={{ height: "auto", width: "100%" }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              disableSelectionOnClick
+              autoHeight
+              pagination={false}
+              getRowId={(row) => row.medical_insurance_id}
+              sx={{
+                "& .MuiDataGrid-columnHeaders": {
+                  backgroundColor: "orange",
+                  color: "white",
+                  fontSize: 16,
+                },
+              }}
+            />
+          </div>
         </CardContent>
       </Card>
       {modal && (
